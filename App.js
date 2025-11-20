2581f0fb202738f93a0fbdff5779e8cb7e9f35e6
mpasha <mpasha@amref.com>
2025-11-20 21:45:08 +0530

    class one

---
+++ App.js
@@ -0,0 +1,11 @@
+function App(){
+      return(
+        <div >
+          <h1 className="color">Hello, React!</h1>
+          <button onClick={()=> alert("zayed is a coder")}>
+            press me daddy</button>
+            </div>);
+
+        };
+      
+        ReactDOM.createRoot(document.getElementById("root")).render(<App />);
