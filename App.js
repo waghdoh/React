42e30a370c91ae49fcd59e71aac729d48a05be9c
wagdoh <mohammedzayed602@gmail.com>
2025-12-30 11:19:34 +0530

    lets vibe

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-function App(){
-      return(
-        <div >
-          <h1 className="color">Hello, React!</h1>
-          <button onClick={()=> alert("zayed is a coder")}>
-            press me daddy</button>
-            </div>);
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-        };
-      
-        ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+function App() {
+  return (
+    <div>
+      <h1 className="color">Hello, React!</h1>
+      <button onClick={() => alert("zayed is a coder")}>
+        press me bro
+      </button>
+    </div>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<App />);
